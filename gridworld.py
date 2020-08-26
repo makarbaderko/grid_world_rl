62be24ec0065e77a909b040363511e61f0b4770a
Makar Baderko <makarbaderko@gmail.com>
2020-08-26 10:53:34 +0300

    Created the Gridworld's main class, that will return states and rewards. (created constructor and functions: set, set_state, current_state)

---
+++ gridworld.py
@@ -0,0 +1,18 @@
+class Grid: #Environment
+	def __init__(self, rows, cols, start):
+		self.rows = rows
+		self.cols = cols
+		self.i = start[0]
+		self.j = start[1]
+	def set(self, rewards, actions):
+		#reward = dict of (i, j) : A (row, col): reward
+		#actions = dict of (i, j) : A (row, col): action list
+		self.rewards = rewards
+		self.actions = actions
+	def set_state(self, s):
+		'''Cheats'''
+		self.i = s[0]
+		self.j = s[1]
+	#What's happening in the gridworld
+	def current_state(self):
+		return (self.i, self.j)
