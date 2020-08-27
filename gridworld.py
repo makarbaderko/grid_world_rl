8886901a6f7099974dab6f7b2590e5caebe7be1a
Makar Baderko <makarbaderko@gmail.com>
2020-08-27 10:01:37 +0300

    Player is allowed to undo_move, we can check for game_over, get all nect posible states, and create basic grid and start playing manually

--- gridworld.py
+++ gridworld.py
@@ -54,4 +54,37 @@ class Grid: #Environment
 			elif action == 'L':
 				self.j -= 1
 		return self.rewards.get((self.i, self.j), 0)
-	
+	def undo_move(self, action):
+		if action == "U":
+			self.i += 1
+		elif action == "D":
+			self.i -= 1
+		elif action == "R":
+			self.j -= 1
+		elif action == "L":
+			self.j += 1
+		#should never happen
+		assert(self.current_state() in self.all_states())
+	def game_over(self):
+		#returns true if the. game if over
+		#and false if not
+		return (self.i, self.j) not in self.actions
+	def all_states(self):
+		#returns all next possible states
+		return set(self.actions.keys()) | set(self.rewards.keys())
+	def standart_grid():
+		g = Grid(3, 4, (2, 0))
+		rewards = {(0, 3): 1, (1, 3): -1}
+		actions = {
+			(0, 0) : ('D', 'R'),
+			(0, 1) : ('L', 'R'),
+			(0, 2) : ('L', 'D', 'R'),
+			(1, 0) : ('U', 'D'),
+			(1, 2) : ('U', 'D', 'R'),
+			(2, 0) : ('U', 'R'),
+			(2, 1) : ('L', 'R'),
+			(2, 2) : ('L', 'R', 'U'),
+			(2, 3) : ('L', 'U'),
+		}
+		g.set(rewards, actions)
+		return g
